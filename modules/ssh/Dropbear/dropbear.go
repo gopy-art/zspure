5ffb1254e58d1078127ff6fb5185037247e463ee
gopy-art <gopyart1@gmail.com>
2026-06-29 15:15:05 +0330

    Add [Dropbear] module in ssh protocol

---
+++ modules/ssh/Dropbear/dropbear.go
@@ -0,0 +1,131 @@
+package dropbear
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"zspure/config"
+	"zspure/config/cmd"
+	"zspure/handler"
+	"zspure/modules/model"
+	"zspure/modules/ssh/os"
+	"zspure/utils"
+)
+
+type Dropbear struct {
+	Category         string                `json:"dvs_category"`
+	DeviceName       string                `json:"device_name"`
+	Version          string                `json:"version"`
+	CveList          []string              `json:"cves"`
+	Sensibility      string                `json:"base_severity"`
+	CveScore         float64               `json:"cve_score"`
+	ExtraInformation model.ModuleExtraInfo `json:"dvs_extra"`
+}
+
+func (d *Dropbear) SetCategory(category ...string) {
+	d.Category = model.Category.Service()
+}
+
+func (d *Dropbear) SetDeviceName(device ...string) {
+	d.DeviceName = "Dropbear"
+}
+
+func (d *Dropbear) Patterns() []map[string]interface{} {
+	return []map[string]interface{}{}
+}
+
+func (d *Dropbear) Filters(banner map[string]interface{}) bool {
+	if banner["server_id"] == nil {
+		return false
+	}
+	if val, ok := banner["server_id"].(map[string]interface{}); ok && val != nil {
+		if v, okk := val["raw"].(string); okk && strings.Contains(strings.ToLower(v), "dropbear") {
+			return true
+		}
+	}
+	return false
+}
+
+func (d *Dropbear) DeviceScan(banner map[string]interface{}) bool {
+	d.ExtraInformation.NewExtraInfo()
+	if ok, result := os.DetectOperatingSystems(banner["server_id"].(map[string]interface{})["raw"].(string)); ok {
+		if result.Name != "" {
+			d.ExtraInformation.SetExtraInfo("operating_system", result.Name)
+		}
+		if result.Version != "" {
+			d.ExtraInformation.SetExtraInfo("os_version", result.Version)
+		}
+	}
+
+	re := regexp.MustCompile(`dropbear_([\d.]+)`)
+	matches := re.FindStringSubmatch(banner["server_id"].(map[string]interface{})["raw"].(string))
+	if len(matches) > 1 {
+		d.Version = matches[1]
+		return true
+	}
+	return false
+}
+
+func (d *Dropbear) CveScan(els *handler.Elastic) {
+	var CVE []model.CVEStructure = make([]model.CVEStructure, 0)
+	var totalScore float64 = 0
+	if config.LOGIC == "execute" {
+		result := utils.RemoveDuplicatesFromMap(els.GatherAllDataInMap(els.CveIndex, "and", map[string]interface{}{
+			"cve.descriptions.value": fmt.Sprintf("%v %v", d.DeviceName, d.Version),
+		}))
+		if len(result) == 0 {
+			return
+		}
+		for _, c := range result {
+			if len(CVE) == 10 {
+				break
+			}
+			cveMod := model.NewCVEStructure(c)
+			CVE = append(CVE, cveMod)
+		}
+	} else if config.FIND_CVE {
+		url := fmt.Sprintf(model.CVE.MainResource(),
+			strings.ToLower(strings.ReplaceAll(fmt.Sprintf("%v %v", d.DeviceName, d.Version), " ", "%20")))
+		recieve, err := utils.GatherCVEOnline(url)
+		if err != nil {
+			cmd.ErrorLogger.Println("[CVE] error in gather the CVE for this device. (Server error)")
+			return
+		}
+		CVE = append(CVE, recieve...)
+	}
+
+	if len(CVE) == 0 {
+		cmd.InfoLogger.Println("[CVE] do not find any CVE for this module.")
+		return
+	}
+
+	for _, v := range CVE {
+		d.CveList = append(d.CveList, v.CVEID)
+		totalScore += v.BaseScore
+	}
+
+	d.CveScore, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", totalScore/float64(len(CVE))), 64)
+	if d.CveScore > 7 {
+		d.Sensibility = "HIGH"
+	} else if d.CveScore >= 4 && d.CveScore <= 7 {
+		d.Sensibility = "MEDIUM"
+	} else if d.CveScore < 4 {
+		d.Sensibility = "LOW"
+	}
+	d.CveList = utils.RemoveDuplicates(d.CveList)
+}
+
+func (d *Dropbear) PrintInfo() string { return model.Category.Service() + " | Dropbear" }
+
+func (d *Dropbear) Result() model.ModuleStructure {
+	return model.ModuleStructure{
+		Category:         d.Category,
+		DeviceName:       d.DeviceName,
+		Version:          d.Version,
+		CveList:          d.CveList,
+		Sensibility:      d.Sensibility,
+		CveScore:         d.CveScore,
+		ExtraInformation: d.ExtraInformation,
+	}
+}
--- modules/ssh/ssh.go
+++ modules/ssh/ssh.go
@@ -2,12 +2,14 @@ package ssh
 
 import (
 	"zspure/modules/model"
+	dropbear "zspure/modules/ssh/Dropbear"
 	openssh "zspure/modules/ssh/OpenSSH"
 )
 
 func NewSSH() []model.ModuleMethods {
 	return []model.ModuleMethods{
 		&openssh.OpenSSH{},
+		&dropbear.Dropbear{},
 	}
 }
 
