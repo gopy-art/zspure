d3a7a26abf0728de30f05485152efa33d4b7a58e
gopy-art <gopyart1@gmail.com>
2026-07-07 21:36:43 +0330

    Add SMTP scanner and detection packet

--- modules/smtp/scanner.go
+++ modules/smtp/scanner.go
@@ -1,21 +1,29 @@
 package smtp
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"net"
+	"strings"
+	"time"
 	"zspure/modules/model"
 )
 
 type SmtpScanning struct {
-	IP     net.IP `json:"ip,omitempty"`
-	Port   int    `json:"port,omitempty"`
-	Status string    `json:"status,omitempty"`
-	Banner string `json:"banner,omitempty"`
-	Sha256 string `json:"sha_256,omitempty"`
+	IP     net.IP     `json:"ip,omitempty"`
+	Port   int        `json:"port,omitempty"`
+	Status string     `json:"status,omitempty"`
+	Banner smtpBanner `json:"banner,omitempty"`
 
 	Description     string `json:"description,omitempty"`
 	DetectionPacket []byte `json:"detection_packet,omitempty"`
 	BannerPacket    []byte `json:"banner_packet,omitempty"`
+}
+
+type smtpBanner struct {
+	Banner string `json:"banner"`
+	Sha256 string `json:"sha256"`
 }
 
 func (s *SmtpScanning) PrintInfo() {
@@ -26,9 +34,63 @@ func (s *SmtpScanning) SetDescription() {
 	s.Description = "SMTP (Simple Mail Transfer Protocol) is the protocol used for sending emails from a client to a server, and between mail servers."
 }
 
-func (s *SmtpScanning) SetAddress(ip net.IP, port int)                {}
-func (s *SmtpScanning) SetDetectionPacket(packet []byte)              {}
-func (s *SmtpScanning) SetBannerPacket(packet []byte)                 {}
-func (s *SmtpScanning) ServiceDetection(conn *net.Conn) (bool, error) { return false, nil }
-func (s *SmtpScanning) BannerGathering(conn *net.Conn) (bool, error)  { return false, nil }
-func (s *SmtpScanning) PrintResult() model.ScanStructure              { return model.ScanStructure{} }
+func (s *SmtpScanning) SetAddress(ip net.IP, port int) {
+	s.IP = ip
+	s.Port = port
+}
+
+func (s *SmtpScanning) SetDetectionPacket(packet []byte) {
+	if len(packet) == 0 || packet == nil {
+		s.DetectionPacket = []byte("HELP\r\n")
+	} else {
+		s.DetectionPacket = packet
+	}
+}
+
+func (s *SmtpScanning) SetBannerPacket(packet []byte) {
+	if len(packet) == 0 || packet == nil {
+		s.BannerPacket = []byte("\r\n")
+	} else {
+		s.BannerPacket = packet
+	}
+}
+
+func (s *SmtpScanning) ServiceDetection(conn *net.Conn) (bool, error) {
+	if _, err := (*conn).Write(s.DetectionPacket); err != nil {
+		return false, fmt.Errorf("Write error: %v", err)
+	}
+	response := make([]byte, 4096)
+	n, err := (*conn).Read(response)
+	if err != nil {
+		return false, fmt.Errorf("Read error: %v", err)
+	}
+	if VerifySMTPContents(string(response[:n])) {
+		s.Banner.Banner = strings.TrimSuffix(string(response[:n]), "\r\n")
+		hash := sha256.Sum256(response[:n])
+		s.Banner.Sha256 = hex.EncodeToString(hash[:])
+		s.Status = "success"
+		return true, nil
+	}
+	s.Status = "failed"
+	return false, nil
+}
+
+func (s *SmtpScanning) BannerGathering(conn *net.Conn) (bool, error) {
+	/*
+		The SMTP protocol is none probe and with the first packet it will give us the informatiom that we want, so in this protocol the ServiceDetection will do everything
+	*/
+	return true, nil
+}
+
+func (s *SmtpScanning) PrintResult() model.ScanStructure {
+	return model.ScanStructure{
+		IP: s.IP,
+		Data: model.ScanDataStructure{
+			Status:    s.Status,
+			Protocol:  "smtp",
+			Port:      s.Port,
+			Result:    s.Banner,
+			Timestamp: time.Now().UTC().Format(time.RFC3339),
+		},
+	}
+}
--- modules/smtp/smtp.go
+++ modules/smtp/smtp.go
@@ -1,8 +1,10 @@
 package smtp
 
 import (
-	devices "zspure/modules/smtp/Devices"
+	"strconv"
+	"strings"
 	"zspure/modules/model"
+	devices "zspure/modules/smtp/Devices"
 )
 
 func NewSmtp() []model.ModuleMethods {
@@ -13,4 +15,29 @@ func NewSmtp() []model.ModuleMethods {
 
 func NewSMTPScanner() *SmtpScanning {
 	return new(SmtpScanning)
+}
+
+func VerifySMTPContents(banner string) bool {
+	if len(banner) < 5 {
+		return false
+	}
+	code, err := strconv.Atoi(banner[0:3])
+	if err != nil {
+		return false
+	}
+	lowerBanner := strings.ToLower(banner)
+	switch {
+	case err == nil && (code < 200 || code >= 300):
+		return false
+	case err == nil,
+		strings.Contains(banner, "SMTP"),
+		strings.Contains(lowerBanner, "blacklist"),
+		strings.Contains(lowerBanner, "abuse"),
+		strings.Contains(lowerBanner, "rbl"),
+		strings.Contains(lowerBanner, "spamhaus"),
+		strings.Contains(lowerBanner, "relay"):
+		return true
+	default:
+		return true
+	}
 }
